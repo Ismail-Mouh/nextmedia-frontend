3e888bb0af3c653484b2fe71ca1f5c7958337fbb
Ismail <mouhismail99@gmail.com>
2021-02-13 22:43:57 +0100

    Product listing

---
+++ src/components/products/Products.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Products list</h1>
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Description</th>
+        <th scope="col">Price</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="product in products" :key="product.id">
+        <td>{{ product.name }}</td>
+        <td>{{ product.description }}</td>
+        <td>{{ product.price }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from '@/utils/axios'
+
+export default {
+  data() {
+    return {
+      products: [],
+    }
+  },
+  computed: {},
+
+  created() {
+    this.getProducts()
+  },
+
+  methods: {
+    getProducts() {
+      axios.get('/products').then((response) => {
+        this.products = response.data
+      })
+    },
+  },
+
+
+}
+</script>
